ad00f64a757ae31652750a52c74166a5767a2f99
barbarauc3m <100495857@alumnos.uc3m.es>
2026-09-04 17:46:30 +0200

    Limpieza de archivos auxiliares

--- output.py
+++
@@ -1,6 +0,0 @@
-# DELETE FILE
-
-import onnxruntime as ort
-session = ort.InferenceSession("service/yolo/model/weights.onnx", providers=["CPUExecutionProvider"])
-print("input:", session.get_inputs()[0].name, session.get_inputs()[0].shape)
-print("output:", session.get_outputs()[0].name, session.get_outputs()[0].shape)
